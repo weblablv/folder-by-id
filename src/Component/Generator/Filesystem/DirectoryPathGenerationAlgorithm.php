488676eaf853c72b479089c7aac9a61dbcc8805e
Edvards Umbrass <edvards.umbrass@gmail.com>
2017-10-01 21:49:08 +0000

    filesystem directories path generator by item id

---
+++ src/Component/Generator/Filesystem/DirectoryPathGenerationAlgorithm.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace WebLabLv\Component\Generator\Filesystem;
+
+final class DirectoryPathGenerationAlgorithm
+{
+    const MAXIMUM_ALLOWED_DEPTH                = 3;
+    const MAXIMUM_DIRECTORIES_ALLOWED_IN_DEPTH = 500;
+
+    /**
+     * @param int $id
+     * @return string
+     */
+    public static function getRelativeDirectoryPathById(int $id): string
+    {
+        $depth = self::MAXIMUM_ALLOWED_DEPTH;
+        $path  = null;
+
+        while(0 !== $depth) {
+            $step           = pow(self::MAXIMUM_DIRECTORIES_ALLOWED_IN_DEPTH, $depth);
+            $stepPath       = null;
+            $currentAttempt = 0;
+
+            while(null === $stepPath) {
+                $minStepValue = ( $step * $currentAttempt ) + 1;
+                $maxStepValue = $step * ( $currentAttempt + 1 );
+
+                if ($id >= $minStepValue && $id <= $maxStepValue) {
+                    $stepPath = sprintf('%s-%s', $minStepValue, $maxStepValue);
+                }
+            }
+
+            $path    .= DIRECTORY_SEPARATOR . $stepPath;
+            $stepPath = null;
+
+            --$depth;
+        }
+
+        return $path;
+    }
+}
